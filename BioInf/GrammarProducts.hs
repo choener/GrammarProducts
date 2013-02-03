9fef199628f35cb2d0504f17c4839ea1c8f10476
Christian Hoener zu Siederdissen <choener@tbi.univie.ac.at>
2013-02-03 20:38:50 +0100

    added examples of grammars we want to parse
    - need comment symbol
    - want to parse productions in different forms
    - need to collect productions with same LHS
    - what about attribute notation?

--- BioInf/GrammarProducts.hs
+++ BioInf/GrammarProducts.hs
@@ -1,2 +1,75 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE GADTs #-}
 
 module BioInf.GrammarProducts where
+
+import Language.Haskell.TH.Quote
+import qualified Language.Haskell.TH as TH
+import Text.Trifecta
+import Control.Applicative
+import Data.Semigroup
+
+
+
+-- | Grammars are represented much like in formal theory as a tuple. We
+-- actually use attribute grammars (Knuth, 1968) to add algebras for
+-- evaluation.
+--
+-- TODO we want to separate N/T in the long run ...
+
+data Grammar = Grammar
+  { nonterms    :: [NT]
+  , terminals   :: [NT]
+  , productions :: [Production]
+  , startSym    :: NT
+  }
+
+-- | Terminals and non-terminals are the same species.
+
+data NT where
+  Terminal :: String -> NT
+  Nonterm  :: String -> NT
+  deriving (Show,Eq)
+
+data Production where
+  Production :: NT -> [[NT]] -> Production
+  deriving (Show)
+
+-- | productions should form a semigroup given equal NT identifier.
+-- Unfortunately, we are not dependently-typed on nt ...
+
+{-
+instance Semigroup Production where
+  (Production a xs) <> (Production b ys)
+    | a==b = Production a (xs ++ ys)
+    | otherwise = error ""
+-}
+
+grammar :: QuasiQuoter
+grammar = QuasiQuoter
+  { quoteExp = grammarExp
+  }
+
+grammarExp :: String -> TH.ExpQ
+grammarExp = fail
+
+data CP where
+  CP :: [NT] -> [[NT]] -> CP
+  deriving (Show)
+
+{-
+p2cp :: Production -> CP
+p2cp (Production x ys) = CP [x] [ys]
+
+cp :: CP -> CP -> CP
+cp (CP a xs) (CP b ys) = CP (a++b) [ x++y | x<-xs, y<- ys ]
+
+t = p2cp $ Production (Nonterm "X") [Nonterm "X", Terminal "u"]
+-}
+
+pN = Nonterm <$> some upper
+pT = Terminal <$> some lower
+--pP = Production <$> pN <* string " -> " <*> sepBy1 (pN <|> pT) ws
+--pPs = foldl (flip (:)) [] <$> sepBy1 pP newline
+
+ws = some $ char ' '
--- MultiAlign.hs
+++ MultiAlign.hs
@@ -1,3 +1,76 @@
+{-# LANGUAGE QuasiQuotes #-}
 
 module Main where
 
+import BioInf.GrammarProducts
+
+main :: IO ()
+main = return ()
+
+
+{-
+test = [grammar|
+
+X -> X u
+X -> Y u
+
+Y -> X -
+Y -> Y -
+
+|]
+-}
+
+-- example we want to be able to parse
+
+{-
+
+-- comments "--" should be comments
+
+-- simple productions. All uppercase strings provide non-terminals. All
+-- lower-case strings provide terminals. Different strings produce different
+-- symbols. This grammar will yield an ADPfusion grammar with "holes" for X,Y
+-- non-terminals. It will also provide "holes" for uu, u, v, z terminals. The
+-- special terminal "-" takes a normal character parser (like "Chr" from
+-- ADPfusion) and allows this parser to advance 0 characters. This is only
+-- really useful in higher-dimensional parsing, where we want to introduce
+-- in-del's
+
+X -> X uu
+X -> u X
+X -> u X v
+X -> u X v Y z
+
+Y -> X u | Y u
+
+-}
+
+{-
+
+-- In addition we want attributes for our grammars. <Char> is of type
+-- Z:.Char:.Char:.Char for 3-cross, and Z:.Char:.Char for 2-sequence alignment.
+
+X -> X u        match :: \xi -> <Char> -> \xi  || \xi is the E-type of the non-terminal table (here: Int)
+X -> Y u
+
+Y -> X -
+Y -> Y -
+
+-}
+
+{-
+
+-- the 2d version
+
+X ->  Xu  |  Xu  |  Yu  |  Yu          ||   f X1X2 <u1,u2> = X1X2 + match (u1,u2)
+X     Xu  |  Yu  |  Xu  |  Yu          ||   f :: Int -> <C,C> -> Int
+
+X ->  Xu  |  Xu  |  Yu  |  Yu
+Y     X-  |  Y-  |  X-  |  Y-
+
+Y ->  X-  |  X-  |  Y-  |  Y-
+X     Xu  |  Yu  |  Xu  |  Yu
+
+Y ->  X-  |  X-  |  Y-  |  Y-
+Y     X-  |  Y-  |  X-  |  Y-
+
+-}
