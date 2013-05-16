761f0be629f267a58f0f5d2b36c01a0aec10d043
Christian Hoener zu Siederdissen <choener@tbi.univie.ac.at>
2013-05-16 18:55:22 +0200

    barebones for the GramProd frontend
    - will provide LaTex and Haskell output

---
+++ GramProd.hs
@@ -0,0 +1,14 @@
+
+-- | The @GramProd@ executable reads a grammatical description (from stdin or a
+-- file) and produces a set of grammars, each written into a separate file.
+--
+-- It is possible to both, produce @LaTeX@ and @Haskell@ output. The Haskell
+-- grammars require "ADPfusion" to be useful -- and you have to provide
+-- algebras that actually evaluate parses.
+
+module Main where
+
+
+
+main :: IO ()
+main = return ()
