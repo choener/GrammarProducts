a53e35aee75d8fc94c8e927e924b57cfc1ffd8aa
Christian Hoener zu Siederdissen <choener@tbi.univie.ac.at>
2015-04-21 11:35:05 +0200

    update to newest FormalGrammars in progress

--- FormalLanguage/GrammarProduct/Op/Common.hs
+++ FormalLanguage/GrammarProduct/Op/Common.hs
@@ -4,6 +4,7 @@ module FormalLanguage.GrammarProduct.Op.Common where
 
 import qualified Data.Set as S
 import Control.Lens
+import Data.Set (Set)
 
 import FormalLanguage.CFG.Grammar
 
@@ -15,23 +16,33 @@ import FormalLanguage.CFG.Grammar
 --
 -- TODO i guess, this collects multidim stuff for now!!!
 
-collectTerminals :: S.Set Rule -> S.Set Symb
-collectTerminals = S.fromList . filter isSymbT . concatMap _rhs . S.toList
+collectTerminals :: S.Set Rule -> S.Set Symbol
+collectTerminals = error "collectTerminals" -- S.fromList . filter isSymbT . concatMap _rhs . S.toList
 
 -- | Collect all non-terminal symbols from a set of rules.
 --
 -- TODO move to FormalGrammars library
 
-collectNonTerminals :: S.Set Rule -> S.Set Symb
-collectNonTerminals = S.fromList . filter isSymbN . concatMap (\r -> r^.lhs : r^.rhs) . S.toList
+collectNonTerminals :: Set Rule -> Set Symbol -- S.Set Rule -> S.Set Symb
+collectNonTerminals = error "collectNonTerminals" -- S.fromList . _ . view (folded.rhs) -- S.fromList . filter isSymbN . concatMap (\r -> r^.lhs : r^.rhs) . S.toList
 
-collectEpsilons :: S.Set Rule -> S.Set TN
+-- |
+--
+-- TODO not needed anymore ?!
+
+collectEpsilons :: S.Set Rule -> S.Set SynTermEps -- TN
+collectEpsilons = error "collectEpsilons"
+{-
 collectEpsilons = S.fromList
                 . filter (\case E -> True ; z -> False)
                 . concatMap (view symb)
                 . concatMap _rhs
                 . S.toList
+-}
 
-genEps :: Symb -> [TN]
-genEps s = replicate (length $ s^.symb) E
+-- | Generate a multidim epsilon symbol of the same length as the given
+-- symbol.
+
+genEps :: Symbol -> Symbol -- Symb -> [TN]
+genEps s = Symbol $ replicate (length $ s^.getSymbolList) Epsilon -- replicate (length $ s^.symb) E
 
