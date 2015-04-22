ca7c62e5a9725b01f1cf9d9ef4934b0a10bd6c7f
Christian Hoener zu Siederdissen <choener@tbi.univie.ac.at>
2015-04-22 03:40:26 +0200

    fixed up Power,Subtract modules

--- FormalLanguage/GrammarProduct/Op/Power.hs
+++ FormalLanguage/GrammarProduct/Op/Power.hs
@@ -1,10 +1,11 @@
 
 module FormalLanguage.GrammarProduct.Op.Power where
 
-import Control.Newtype
+--import Control.Newtype
 import Data.Semigroup
 import Control.Lens
 import Control.Lens.Fold
+import Data.Set.Lens
 import qualified Data.Set as S
 import Data.List (genericReplicate)
 import Text.Printf
@@ -18,11 +19,16 @@ import FormalLanguage.CFG.Grammar
 power :: Grammar -> Integer -> Grammar
 power g k
   | k < 1     = error $ "Op.Power.power: power " ++ show k ++ " < 1"
-  | otherwise = over (lens) (what) g
+  | otherwise = over (rules . setmapped . attr) kAttr
+              . over (rules . setmapped . rhs . traverse) kDim
+              . over (rules . setmapped . lhs) kDim
+              . over start kDim
+              $ g
   where kDim :: Symbol -> Symbol
-        kDim = undefined
-        kAttr :: AttributeFunction -> AttributeFunction
-        kAttr = undefined
+        kDim = Symbol . concat . genericReplicate k . _getSymbolList
+        kAttr :: [AttributeFunction] -> [AttributeFunction]
+        kAttr = concat . genericReplicate k
+
 {-
 power g k = Grammar ts ns es rs s nm where
   ts = g^.tsyms
--- FormalLanguage/GrammarProduct/Op/Subtract.hs
+++ FormalLanguage/GrammarProduct/Op/Subtract.hs
@@ -2,17 +2,19 @@
 
 module FormalLanguage.GrammarProduct.Op.Subtract where
 
-import Control.Newtype
-import Data.Semigroup
-import Control.Lens
-import Control.Lens.Fold
+import           Control.Arrow ((&&&))
+import           Control.Lens.Fold
+import           Control.Lens hiding (outside)
+import           Control.Newtype
+import           Data.List (genericReplicate)
+import           Data.Semigroup
+import qualified Data.Map as M
 import qualified Data.Set as S
-import Data.List (genericReplicate)
-import Text.Printf
+import           Text.Printf
 
-import FormalLanguage.CFG.Grammar
+import           FormalLanguage.CFG.Grammar
 
-import FormalLanguage.GrammarProduct.Op.Common
+import           FormalLanguage.GrammarProduct.Op.Common
 
 
 
@@ -20,7 +22,20 @@ import FormalLanguage.GrammarProduct.Op.Common
 
 subtract :: Grammar -> Grammar -> Grammar
 subtract l r
-    | gDim l /= gDim r = error $ printf "grammars %s and %s have different dimensions, cannot unify. (subtract)" (show l) (show r)
+    | dim l /= dim r           = error $ printf "grammars %s and %s have different dimensions, cannot unify. (subtract)" (show l) (show r)
+    | l^.outside /= r^.outside = error $ printf "grammars %s and %s have different inside/outside annotation." (show l) (show r)
+    | otherwise                = g
+    where sv = M.fromList . map ((_name &&& id) . fst) . uniqueSynVarsWithTape  $ g
+          st = M.fromList . map ((_name &&& id) . fst) . uniqueSynTermsWithTape $ g
+          tv = M.fromList . map ((_name &&& id) . fst) . uniqueTermsWithTape    $ g
+          io = l^.outside
+          rs = (l^.rules) S.\\ (r^.rules)
+          s  = if (anyOf (folded . lhs) ((l^.start) ==) rs) then l^.start else Symbol []
+          p  = M.union (l^.params) (r^.params)
+          g  = Grammar sv st tv io rs s p (l^.grammarName ++ r^.grammarName) False
+
+{-
+subtract l r
     | otherwise        = Grammar ts ns es rs s (l^.name ++ r^.name) where
         ts = collectTerminals rs
         ns = collectNonTerminals rs
@@ -31,4 +46,5 @@ subtract l r
                Just s' -> if anyOf (rules.folded.lhs) (==s') l
                             then l^.start
                             else Nothing
+-}
 
