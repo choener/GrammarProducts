ea7d901c5fd70e82e63736bf366b06434844a686
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2019-03-04 00:16:07 +0100

    move to envhs

--- default.nix
+++ default.nix
@@ -1,2 +1,21 @@
-with (import <nixpkgs> {});
-hsDevFunctions ./.
+{ mkDerivation, ADPfusion, ansi-wl-pprint, base, bytestring
+, containers, data-default, FormalGrammars, lens, newtype, parsers
+, PrimitiveArray, semigroups, stdenv, template-haskell
+, transformers, trifecta
+}:
+mkDerivation {
+  pname = "GrammarProducts";
+  version = "0.1.1.4";
+  src = ./.;
+  isLibrary = true;
+  isExecutable = true;
+  libraryHaskellDepends = [
+    ADPfusion ansi-wl-pprint base bytestring containers data-default
+    FormalGrammars lens newtype parsers PrimitiveArray semigroups
+    template-haskell transformers trifecta
+  ];
+  testHaskellDepends = [ base ];
+  homepage = "https://github.com/choener/GrammarProducts";
+  description = "Grammar products and higher-dimensional grammars";
+  license = stdenv.lib.licenses.gpl3;
+}
