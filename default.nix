29ac74d8549593e343ed7b3bf4d0e671526a8630
Christian Hoener zu Siederdissen <christian.hoener.zu.siederdissen@uni-jena.de>
2022-05-03 16:06:54 +0200

    updated default.nix

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
-{ mkDerivation, ADPfusion, ansi-wl-pprint, base, bytestring
-, containers, data-default, FormalGrammars, lens, newtype, parsers
-, PrimitiveArray, semigroups, stdenv, template-haskell
-, transformers, trifecta
+{ mkDerivation, ADPfusion, base, bytestring, containers
+, data-default, FormalGrammars, lens, lib, newtype, parsers
+, PrimitiveArray, semigroups, template-haskell, transformers
+, trifecta
 }:
 mkDerivation {
   pname = "GrammarProducts";
@@ -10,12 +10,12 @@ mkDerivation {
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
-    ADPfusion ansi-wl-pprint base bytestring containers data-default
-    FormalGrammars lens newtype parsers PrimitiveArray semigroups
-    template-haskell transformers trifecta
+    ADPfusion base bytestring containers data-default FormalGrammars
+    lens newtype parsers PrimitiveArray semigroups template-haskell
+    transformers trifecta
   ];
   testHaskellDepends = [ base ];
   homepage = "https://github.com/choener/GrammarProducts";
   description = "Grammar products and higher-dimensional grammars";
-  license = stdenv.lib.licenses.gpl3;
+  license = lib.licenses.gpl3Only;
 }
